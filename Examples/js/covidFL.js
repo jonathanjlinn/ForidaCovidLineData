f2a886e5df2cc73ca7469800b6acaf1945bfe8b0
Jonathan Linn <jonathanjlinn@gmail.com>
2020-05-22 22:52:44 -0400

    Florida

--- Examples/js/covidFL.js
+++ Examples/js/covidFL.js
@@ -153,7 +153,7 @@
 
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
-        // translateButton();
+        translateButton();
         $("#submitButton").click(function() {
             tableau.connectionName = "Florida COVID Line Data"; // This will be the data source name in Tableau
             tableau.submit(); // This sends the connector object to Tableau
@@ -164,35 +164,35 @@
 // Values attached to the tableau object are loaded asyncronously.
 // Here we poll the value of locale until it is properly loaded
 // and defined, then we turn off the polling and translate the text.
-// var translateButton = function() {
-//     var pollLocale = setInterval(function() {
-//         if (tableau.locale) {
-//             switch (tableau.locale) {
-//                 case tableau.localeEnum.china:
-//                     $("#submitButton").text("获取地震数据");
-//                     break;
-//                 case tableau.localeEnum.germany:
-//                     $("#submitButton").text("Erhalten Erdbebendaten!");
-//                     break;
-//                 case tableau.localeEnum.brazil:
-//                     $("#submitButton").text("Obter Dados de Terremoto!");
-//                     break;
-//                 case tableau.localeEnum.france:
-//                     $("#submitButton").text("Obtenir les Données de Séismes!");
-//                     break;
-//                 case tableau.localeEnum.japan:
-//                     $("#submitButton").text("地震データの取得");
-//                     break;
-//                 case tableau.localeEnum.korea:
-//                     $("#submitButton").text("지진 데이터 가져 오기");
-//                     break;
-//                 case tableau.localeEnum.spain:
-//                     $("#submitButton").text("Obtener Datos de Terremotos!");
-//                     break;
-//                 default:
-//                     $("#submitButton").text("Get Earthquake Data!");
-//             }
-//             clearInterval(pollLocale);
-//         }
-//     }, 10);
-// };
+var translateButton = function() {
+    var pollLocale = setInterval(function() {
+        if (tableau.locale) {
+            switch (tableau.locale) {
+                case tableau.localeEnum.china:
+                    $("#submitButton").text("获取地震数据");
+                    break;
+                case tableau.localeEnum.germany:
+                    $("#submitButton").text("Erhalten Erdbebendaten!");
+                    break;
+                case tableau.localeEnum.brazil:
+                    $("#submitButton").text("Obter Dados de Terremoto!");
+                    break;
+                case tableau.localeEnum.france:
+                    $("#submitButton").text("Obtenir les Données de Séismes!");
+                    break;
+                case tableau.localeEnum.japan:
+                    $("#submitButton").text("地震データの取得");
+                    break;
+                case tableau.localeEnum.korea:
+                    $("#submitButton").text("지진 데이터 가져 오기");
+                    break;
+                case tableau.localeEnum.spain:
+                    $("#submitButton").text("Obtener Datos de Terremotos!");
+                    break;
+                default:
+                    $("#submitButton").text("Get Earthquake Data!");
+            }
+            clearInterval(pollLocale);
+        }
+    }, 10);
+};
