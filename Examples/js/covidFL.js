87a59880e1c058cf0449b5e3b027cbc10aabf0b8
Jonathan Linn <jonathanjlinn@gmail.com>
2020-10-07 11:19:46 -0400

    Update covidFL.js

--- Examples/js/covidFL.js
+++ Examples/js/covidFL.js
@@ -82,7 +82,7 @@
     // Download the data
     myConnector.getData = function(table, doneCallback) {
         
-        var maxVal = 650000;
+        var maxVal = 900000;
         var incrementalVal = 2000;
         var lowerBound = 0;
         var upperBound = 2000;
